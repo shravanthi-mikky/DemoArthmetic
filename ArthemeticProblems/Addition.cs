8b134fc57b1acfa0250efb7e449171d3c34d5860
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-12-01 15:27:08 +0530

    [shravanthi] - Addition

---
+++ ArthemeticProblems/Addition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArthemeticProblems
+{
+    public class Addition
+    {
+        public int AdditionMethod(int a, int b)
+        {
+            int c = a+ b;
+            return c;
+
+        }
+    }
+}
--- ArthemeticProblems/Program.cs
+++ ArthemeticProblems/Program.cs
@@ -7,6 +7,9 @@ namespace ArthemeticProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Performing Arthematic Operation");
+            Addition addition = new Addition();
+            int addResult= addition.AdditionMethod(2, 4);
+            Console.WriteLine(addResult);
         }
     }
 }
