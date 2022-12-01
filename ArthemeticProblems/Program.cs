0f16dad1ac9582febb2d495163f1d0082ad94240
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-12-01 15:41:08 +0530

    [shravanthi] - Subraction

--- ArthemeticProblems/Program.cs
+++ ArthemeticProblems/Program.cs
@@ -7,9 +7,27 @@ namespace ArthemeticProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Performing Arthematic Operation");
-            Addition addition = new Addition();
-            int addResult= addition.AdditionMethod(2, 4);
-            Console.WriteLine(addResult);
+            
+
+            while (true)
+            {
+                Console.WriteLine("Enter the option to perform the operation \n1) Addition \n2) Subraction");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        Addition addition = new Addition();
+                        int addResult = addition.AdditionMethod(2, 4);
+                        Console.WriteLine(addResult);
+                        break;
+                    case 2:
+                        int subResult = Subraction.SubractionMethod(5, 8);
+                        Console.WriteLine("Result of subraction is :"+ subResult);
+                        break;
+                }
+            }
+
+
         }
     }
 }
---
+++ ArthemeticProblems/Subraction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArthemeticProblems
+{
+    public class Subraction
+    {
+        public static int SubractionMethod(int a,int b)
+        {
+            int c = b - a;
+            return c;
+        }
+    }
+}
