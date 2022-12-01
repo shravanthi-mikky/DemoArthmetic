83fd882bd8ba388d6893d3b37b10702882677128
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-12-01 15:22:31 +0530

    [shravanthi] - Arthematic Operations

---
+++ ArthemeticProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArthemeticProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Performing Arthematic Operation");
+        }
+    }
+}
